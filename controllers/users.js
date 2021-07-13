dbbcf4f646beb411c9f705518dc660f57c36bbf6
pg0817h <Abby@Geonhyeongs-MacBook-Air.local>
2021-07-12 23:42:28 -0700

    test/abby

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const { Users } = require("../models");
-
+const admin = require("../firebase/firebase.js");
 exports.add = async (req, res) => {
   const { email } = req.body;
  
@@ -16,3 +16,17 @@ exports.add = async (req, res) => {
     return res.status(500).json(err);
   }
 };
+//test firebase 
+exports.loginStatus = async (req, res) => {
+  console.log("test firebase")
+  
+  try {
+    const firebaseUser = await admin
+      .auth()
+      .getUser("ASNaAKiQZBe3bd03dQPD2XNwdrS2")
+      .then((data) => console.log("backend firebase test",data))
+  } catch (err) {
+    console.log("firebase test error",err)
+  }
+};
+
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const { add } = require("../controllers/users");
+const { add,loginStatus } = require("../controllers/users");
 const { authCheck, adminCheck } = require("../middleware/auth");
 
 //router.post("/", authCheck, adminCheck, add);
 router.post("/", add);
+router.get("/test",loginStatus);
 module.exports = router;
